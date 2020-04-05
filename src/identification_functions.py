eb8c11de0d862236e82fe25219535faa722dbdad
PedroHRPBS <pedro.silva@ku.ac.ae>
2020-04-05 12:56:02 +0400

    Generation input layer. Working. Tested.

--- src/identification_functions.py
+++ src/identification_functions.py
@@ -19,8 +19,8 @@ h_mrft = 0.04 #Change this depending on the defined amplitude of MRFT
 def receive_data(t_pv, t_u, t_time):
 
         MRFT_command.append(t_u)
-        MRFT_error.append(0.0-t_pv) #Error = Reference - PV
-        # MRFT_time.append(t_time)
+        MRFT_error.append(t_pv)
+        MRFT_time.append(t_time)
 
         # print(t_time)
         # print("U:",t_u)
@@ -63,13 +63,71 @@ def detect_steady_state(signal_start, signal_end, samples=3):
                         print("Steady State DETECTED")
                         control_timeseries = list(itertools.islice(MRFT_command, signal_start, signal_end-1)) #-1 to remove the last rise edge
                         error_timeseries = list(itertools.islice(MRFT_error, signal_start, signal_end-1))
-                        # timeseries = list(itertools.islice(MRFT_time, signal_start, signal_end-1))
+                        timeseries = list(itertools.islice(MRFT_time, signal_start, signal_end-1))
 
                         assert(len(control_timeseries) == len(error_timeseries))
 
-                        print(control_timeseries)
-                        print(error_timeseries)
+                        #print(control_timeseries)
+                        #print(error_timeseries)
                         #print(timeseries)
-                                
-def normalize_data(t_control_timeseries, error_timeseries):
-        pass
+
+                        interpolate_data(control_timeseries, error_timeseries, timeseries)
+
+def interpolate_data(control_timeseries, error_timeseries, timeseries):
+        #Interpolate
+        initial_value = timeseries[0]
+        x = [(i-initial_value)*1000 for i in timeseries] 
+
+        xvals = np.linspace(x[0], x[-1], round(x[-1]))  #1ms interpolation
+        error2_interp = np.interp(xvals, x, error_timeseries)
+        control2_interp = np.interp(xvals, x, control_timeseries)
+
+        #Removing transition values
+        control2_interp[control2_interp>=0] = max(control2_interp)
+        control2_interp[control2_interp<0] = min(control2_interp)
+
+        control2_interp = control2_interp.tolist()
+        error2_interp = error2_interp.tolist()
+
+        assert(len(error2_interp) == len(control2_interp) == len(xvals))
+
+        normalize_data(control2_interp, error2_interp)
+
+
+def normalize_data(control_timeseries, error_timeseries):
+        sample_size = 2260
+        h_mrft_control = (max(control_timeseries)-min(control_timeseries)) / 2.0
+        h_mrft_error = (max(error_timeseries)-min(error_timeseries)) / 2.0
+
+        scaled_gain =  h_mrft_control / h_mrft_error
+
+        #Zero center
+        offset_control = ((max(control_timeseries) + min(control_timeseries)) / 2.0)
+        offset_error = ((max(error_timeseries) + min(error_timeseries)) / 2.0)
+        control_timeseries_zero_center = [x-offset_control for x in control_timeseries]  
+        error_timeseries_zero_center = [x-offset_error for x in error_timeseries]
+
+        #Normalize
+        control_timeseries_max = max(control_timeseries_zero_center)
+        error_timeseries_max = max(error_timeseries_zero_center)
+        control_timeseries_normalized = [x * 1.0 / control_timeseries_max for x in control_timeseries_zero_center]
+        error_timeseries_normalized = [x * 1.0 / error_timeseries_max for x in error_timeseries_zero_center]
+
+
+        normalized_control_timeseries = np.zeros(sample_size)
+        normalized_error_timeseries = np.zeros(sample_size)
+
+        #Zero-padding
+        normalized_control_timeseries[-len(control_timeseries_normalized):] = control_timeseries_normalized
+        normalized_error_timeseries[-len(error_timeseries_normalized):] = error_timeseries_normalized
+
+        #Concatenate
+        input_layer = np.concatenate((normalized_error_timeseries, normalized_control_timeseries), axis=0)
+
+        input_layer = input_layer.tolist()
+
+        print(input_layer)
+       
+
+
+        
